35ff8f8fe36904b0a9e654cdc79ef7b2151dae22
bamason <beth@waffle.io>
2018-04-20 13:03:04 -0600

    deployments!

--- deploy.js
+++ deploy.js
@@ -1,1 +1,30 @@
-console.log('DEPLOYED!');
+const request = require('request-promise-native');
+
+let sha = process.env.TRAVIS_PULL_REQUEST_SHA
+
+const run = async (sha) => {
+  let response = await request.post({
+    url: `https://api.github.com/repos/AnaMSteele/Demo-testing-/deployments?access_token=${process.env.GITHUB_ACCESS_TOKEN}`,
+    headers: {
+      'User-Agent': 'Waffle.io demo'
+    },
+    body: {
+      ref: sha,
+      auto_merge: false,
+    },
+    json: true
+  })
+
+  await request.post({
+    url: `https://api.github.com/repos/AnaMSteele/Demo-testing-/deployments/${response.id}/statuses?access_token=${process.env.GITHUB_ACCESS_TOKEN}`,
+    headers: {
+      'User-Agent': 'Waffle.io demo'
+    },
+    body: {
+      state: 'success',
+    },
+    json: true
+  })
+};
+
+run(sha)
